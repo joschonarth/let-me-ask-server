e6eee776402c13075df65d487a49dbcf642810e5
joschonarth <joschonarth@gmail.com>
2026-03-11 23:03:29 -0300

    fix: update audio chunks embeddings vector dimension to 3072

--- src/db/migrations/0002_lame_siren.sql
+++ src/db/migrations/0002_lame_siren.sql
@@ -2,7 +2,7 @@ CREATE TABLE "audio_chunks" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"room_id" uuid NOT NULL,
 	"transcription" text NOT NULL,
-	"embeddings" vector(768) NOT NULL,
+	"embeddings" vector(3072) NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
